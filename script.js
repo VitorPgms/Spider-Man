7b8955b76e8bde66850f75a52f18265cebd4a355
Vitor de Paula Gomes <112875256+VitorPgms@users.noreply.github.com>
2023-06-26 17:56:33 -0300

    Slider atualizado
    Atualizando o Slider sem o Swiper

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    centeredSlides: false,
-    slidesPerGroupSkip: 1,
-    grabCursor: true,
-    keyboard: {
-      enabled: true,
-    },
-    breakpoints: {
-      769: {
-        slidesPerView: 2,
-        slidesPerGroup: 2,
-      },
-    },
-    scrollbar: {
-      el: ".swiper-scrollbar",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+const sliderContainer = document.querySelector('.slider-container');
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+const slideWidth = slides[0].offsetWidth + 20; // Considerando a margem de 10px em cada lado
+const visibleSlides = 3;
+const totalSlides = slides.length;
+
+let currentSlide = 0;
+
+slider.style.width = `${slideWidth * totalSlides}px`;
+
+function showSlide() {
+  const startPosition = -currentSlide * slideWidth + (sliderContainer.offsetWidth - slideWidth) / 2;
+  slider.style.transform = `translateX(${startPosition}px)`;
+}
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide > totalSlides - visibleSlides) {
+    currentSlide = totalSlides - visibleSlides;
+  }
+  showSlide();
+}
+
+function prevSlide() {
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = 0;
+  }
+  showSlide();
+}
+
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
+
+showSlide();
